e5edc5a6160eeae9f3509d8e2d582bc5e8ba1657
Jeongho Yun <yjh@MacBook-Pro.local>
2020-11-03 00:40:34 +0900

    JavaScript study
    study JS

---
+++ JSstudy01/study01/index.js
@@ -0,0 +1,18 @@
+//alert('Im studying!! This is JS')
+
+console.log("this is test");
+
+//Variable
+//JS는 가능한 우리가 입력한 값들의 타입이 무엇인지 값들을 통해 판단한다.
+
+//initialization
+//create
+//use
+
+let a = 221;
+let b = a - 5;
+a = 4; //변수를 실행하지 않고, 업데이트 시킴
+
+console.log(b, a);
+
+//What is let?
