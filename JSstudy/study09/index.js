c909f7a155990b13803cad6bf049d617c7ae86ae
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-08 22:51:40 +0900

    Update index.js

--- JSstudy/study09/index.js
+++ JSstudy/study09/index.js
@@ -4,12 +4,18 @@ const title = document.querySelector("#titleTest");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentClass = title.className;
-  if (currentClass !== CLICKED_CLASS) {
-    title.className = CLICKED_CLASS;
-  } else {
-    title.className = "";
-  }
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // //contain은 해당 값을 가지고 있냐 없냐를 판단하여 true or false를 반환한다.
+  // const currentClass = title.className;
+  // if (hasClass) {
+  //   title.classList.remove(CLICKED_CLASS);
+  // } else {
+  //   title.classList.add(CLICKED_CLASS);
+  // }
+
+  //toggle 사용시 간단하게 수정이 가능하다.
+
+  title.classList.toggle(CLICKED_CLASS);
 }
 function init() {
   title.addEventListener("click", handleClick);
